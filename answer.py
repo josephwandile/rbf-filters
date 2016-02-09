cc89f9580cbfd130079cbdc4cb07649d80c8989e
Joseph Kahn <josephkahn@college.harvard.edu>
2016-02-09 12:16:22 -0500

    more cleanup + added TODOs

--- answer.py
+++ answer.py
@@ -3,55 +3,29 @@ import numpy as np
 import scipy
 import random as r
 import math
-from itertools import chain
 
-# Vertices. Feeding in tuples. Poggio and Edelman paper is the one we're
-# doing. Just use the corners.
+# TODOs
+# 0. Update euclidean distance formula.
+# 1. Limitations. Four corners. Aligned
+# 2. Dynamically label generated and test data. The corner closest to the top left should be the top left corner for example.
+# 3. Extend by considering the angles and side lengths instead of just the locations of the vertices.
 
-# Inputs must be labeled. Top left. Top right. etc...
-
-# Bring up limitations in presentation like needing alignment, labeled
-# features...
-
-# Append it all and take euclidean distance
+# Calculate euclidean distance between prototype vector and test vector
 # x1 y1 x2 y2 x3 y3 x4 y4
 # a1 b1 a2 b2 a3 b3 a4 b4
 
-IMAGE_EDGE_LENGTH = 200  # we expect each image to be 200px x 200px
-IMAGE_FEATURE_LENGTH = IMAGE_EDGE_LENGTH ** 2
 LAM = 0.0001  # 'lambda' is a reserved keyword in python
 SIGMA = 1
 PI = math.pi
 
-# NOTE: PLEASE PLEASE PLEASE Make sure to confuse arrays and lists in comments
+class Wireframe():
 
-# NOTE: NOT ENTIRELY SURE
-#
-# My understanding thus far is that there are a series of RBF Units
-# (one for each training example in our case). To test something, you take
-# the euclidean distance between the image and the training image then you you
-# weight all of those values (using the weight equation given in the HW) and see
-# if it passes a threshold
-
-# NOTE: UPDATED: STILL NOT ENTIRELY SURE
-#
-# Still not entirely sure, but starting to think this is simply l2-regularized
-# linear regression, where the feature vector is the distance from the 'centers'
-# (which are actually just all of the training points)
-
-class square():
-
-    def __init__(self, shape_type='square'):
+    def __init__(self, vertices=[(0, 1), (1, 1), (0, 0), (1, 0)]):
         """
-        | tl  tr |
-        | bl  br |
-
         self.tl, self.tr, self.bl, self.br = self.vertices
         """
 
-        # Instantiate column vectors
-        self.vertices = [(0, 1), (1, 1), (0, 0), (1, 0)]
-        self.vector_rep = np.matrix([[],[]])
+        self.vertices = vertices
 
         self._rotate()
         self._scale()
@@ -80,7 +54,8 @@ class square():
     def _scale(self, factor=None):
 
         if factor is None:
-            factor = math.ceil(r.random() * 50)
+            random = r.random() * 50
+            factor = random if random >= 1 else random + 1
 
         T = factor, 0, 0, factor
 
@@ -97,6 +72,7 @@ class square():
             new_vertex = x + translate_x, y + translate_y
             self.vertices[i] = new_vertex
 
+
     def draw(self):
 
         # I don't want to talk about what I did here. Just go with it. My god. Bad coders unite.
@@ -106,6 +82,13 @@ class square():
         pylab.scatter(xs, ys, marker='.', s=5, c='black')
         pylab.show()
 
+
+class Square(Wireframe):
+
+    def _label_vertices():
+
+        return
+        # TODO relabel the vertices (i.e. correcl order them in the output vector) after the linear transformations in super.init
 
 class RBF(object):
     """
@@ -143,5 +126,5 @@ class RBF(object):
         G = self._compute_G(vector_data)
         return np.dot(G, self.weights)
 
-test = square()
-test.draw()
+square = Square()
+square.draw()
